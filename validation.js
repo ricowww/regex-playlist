00ce19013ec6d58c1d852c7f193c38151e9e13e4
Rico <rico.delapena.banzon@gmail.com>
2021-12-03 19:02:14 +0800

    update

--- validation.js
+++ validation.js
@@ -1,1 +1,21 @@
 // validation script here
+//var reg = /[a-z]/gi;
+
+const inputs = document.querySelectorAll('input');
+
+//telephone number field
+const patterns = {
+    telephone:/^\d{11}$/
+}; 
+
+//validation function
+function validate(field.regex){
+    console.regex.test(field.value)
+}
+
+inputs.forEach((input)=> {
+    input.addEventListener('keyup',(e) => {
+        //console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[])
+    });
+});
